82c07ac08529d502ea8e6bd11650c242bf494cf7
Sung Won Cho <mikeswcho@gmail.com>
2018-09-25 11:37:29 +1000

    Migrate edit_note from v2 to v3

--- migrate/migrations.go
+++ migrate/migrations.go
@@ -60,3 +60,59 @@ var m1 = migration{
 		return nil
 	},
 }
+
+var m2 = migration{
+	name: "upgrade-edit-note-from-v2-to-v3",
+	run: func(tx *sql.Tx) error {
+		rows, err := tx.Query("SELECT uuid, data FROM actions WHERE type = ? AND schema = ?", "edit_note", 2)
+		if err != nil {
+			return errors.Wrap(err, "querying rows")
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var uuid, dat string
+
+			err = rows.Scan(&uuid, &dat)
+			if err != nil {
+				return errors.Wrap(err, "scanning a row")
+			}
+
+			var oldData actions.EditNoteDataV2
+			err = json.Unmarshal([]byte(dat), &oldData)
+			if err != nil {
+				return errors.Wrap(err, "unmarshalling existing data")
+			}
+
+			var bookUUID *string
+			if oldData.ToBook != nil {
+				var dst string
+				err = tx.QueryRow("SELECT uuid FROM books WHERE label = ?", *oldData.ToBook).Scan(&dst)
+				if err != nil {
+					return errors.Wrap(err, "scanning book uuid")
+				}
+
+				bookUUID = &dst
+			}
+
+			newData := actions.EditNoteDataV3{
+				NoteUUID: oldData.NoteUUID,
+				BookUUID: bookUUID,
+				Content:  oldData.Content,
+				Public:   oldData.Public,
+			}
+
+			b, err := json.Marshal(newData)
+			if err != nil {
+				return errors.Wrap(err, "marshalling new data")
+			}
+
+			_, err = tx.Exec("UPDATE actions SET data = ?, schema = ? WHERE uuid = ?", string(b), 3, uuid)
+			if err != nil {
+				return errors.Wrap(err, "updating a row")
+			}
+		}
+
+		return nil
+	},
+}
