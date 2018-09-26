9ab28e06b30732fbe34c603602b882770675e6d3
Sung Won Cho <mikeswcho@gmail.com>
2018-09-26 19:08:13 +1000

    Fix test

--- main_test.go
+++ main_test.go
@@ -103,9 +103,9 @@ func TestAddNote_NewBook_ContentFlag(t *testing.T) {
 	}
 
 	testutils.AssertNotEqual(t, noteAction.Timestamp, 0, "noteAction timestamp mismatch")
-	testutils.AssertEqual(t, noteAction.Schema, 3, "noteAction timestamp mismatch")
+	testutils.AssertEqual(t, noteAction.Schema, 3, "noteAction schema mismatch")
 	testutils.AssertNotEqual(t, bookAction.Timestamp, 0, "bookAction timestamp mismatch")
-	testutils.AssertEqual(t, bookAction.Schema, 2, "noteAction timestamp mismatch")
+	testutils.AssertEqual(t, bookAction.Schema, 2, "bookAction schema mismatch")
 
 	testutils.AssertNotEqual(t, bookActionData.BookName, "", "bookAction data note_uuid mismatch")
 	testutils.AssertNotEqual(t, bookActionData.BookUUID, "", "bookAction data book_uuid mismatch")
@@ -147,17 +147,19 @@ func TestAddNote_ExistingBook_ContentFlag(t *testing.T) {
 		db.QueryRow("SELECT uuid, content, added_on FROM notes WHERE book_uuid = ? AND content = ?", "js-book-uuid", "foo"), &n2.UUID, &n2.Content, &n2.AddedOn)
 	var noteAction actions.Action
 	testutils.MustScan(t, "getting note action",
-		db.QueryRow("SELECT data, timestamp FROM actions WHERE type = ?", actions.ActionAddNote), &noteAction.Data, &noteAction.Timestamp)
+		db.QueryRow("SELECT data, schema, timestamp FROM actions WHERE type = ?", actions.ActionAddNote), &noteAction.Data, &noteAction.Schema, &noteAction.Timestamp)
 
 	var noteActionData actions.AddNoteDataV3
 	if err := json.Unmarshal(noteAction.Data, &noteActionData); err != nil {
 		log.Fatalf("unmarshalling the action data: %s", err)
 	}
 
+	testutils.AssertNotEqual(t, noteAction.Timestamp, 0, "action timestamp mismatch")
+	testutils.AssertEqual(t, noteAction.Schema, 3, "action schema mismatch")
+
 	testutils.AssertEqual(t, noteActionData.Content, "foo", "action data name mismatch")
 	testutils.AssertNotEqual(t, noteActionData.NoteUUID, "", "action data note_uuid mismatch")
 	testutils.AssertEqual(t, noteActionData.BookUUID, "js-book-uuid", "action data book_uuid mismatch")
-	testutils.AssertNotEqual(t, noteAction.Timestamp, 0, "action timestamp mismatch")
 	testutils.AssertNotEqual(t, n1.UUID, "", "Note should have UUID")
 	testutils.AssertEqual(t, n1.Content, "Booleans have toString()", "Note content mismatch")
 	testutils.AssertEqual(t, n1.AddedOn, int64(1515199943), "Note added_on mismatch")
